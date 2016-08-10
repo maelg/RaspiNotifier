8cc463631468841457f3e58ad17aeb9acce3e719
Mael Guillossou <mael.guillossou@gmail.com>
2016-08-10 20:16:08 +0200

    Syntax error

--- checker/gmail.py
+++ checker/gmail.py
@@ -31,7 +31,7 @@ def get_credentials(createCredential):
             else: # Needed only for compatibility with Python 2.6
                 credentials = tools.run(flow, store)
             print('Storing credentials to ' + credential_path)
-        else
+        else:
             credentials = None
     return credentials
 
